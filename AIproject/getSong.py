d82d4b5468bbf7d5f390a778202ae1a586396c76
XiaoyuCong <xiaoyuco@andrew.cmu.edu>
2019-04-02 22:39:16 -0700

    recommend 5 music

--- AIproject/getSong.py
+++ AIproject/getSong.py
@@ -7,21 +7,39 @@ import sys
 #get a song from music.csv based on picture_valence and picture_arsoual
 #return : the title of the song
 def get_song(picture_valence, picture_arousal):
+    result = {}
+
     picture_valence = picture_valence * 5 + 5
     picture_arousal = picture_arousal * 5 + 5
     data = pd.read_csv('music.csv')
     num = int(data.describe().ix[0, 0])
-    min_diff = 100
-    song_title = ""
-    for i in range(num):
+    #min_diff = 100
+
+    for i in range(5):
+        record = data.ix[i,:]
+        diff = abs(picture_valence - record['valence_mean']) + abs(picture_arousal - record['arousal_mean'])
+        result[record['Song title']] = (diff,record['Artist'])
+    #song_title = ""
+    for i in range(5,num):
         record = data.ix[i, :]
         diff = abs(picture_valence - record['valence_mean']) + abs(picture_arousal - record['arousal_mean'])
-        if min_diff > diff:
-            min_diff = diff
-            song_title = record['Song title']
-
-    print(song_title)
-    return  song_title
+        max_song = result.keys()[0]
+        max_diff = result[max_song][0]
+        for song in result:
+            if result[song][0] > max_diff:
+                max_song = song
+                max_diff = result[song][0]
+        #print(len(result))
+        if diff < max_diff:
+            del result[max_song]
+            result[record['Song title']] = (diff, record['Artist'])
+    #print(result)
+    result_string = ""
+    for song in result:
+        result_string += song + '-' + result[song][1]+'/'
+    print(result_string)
+    #print(song_title)
+    return  result_string
 
 
 get_song(float(sys.argv[1]),float(sys.argv[2]))
