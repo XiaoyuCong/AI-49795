61bf5d4414fb9ca8ef31d0e3c416945ec8f004d1
Yuxin Liang <yuxinlia@andrew.cmu.edu>
2019-04-15 07:52:26 -0700

    minor change

--- AIproject/app.js
+++ AIproject/app.js
@@ -3,63 +3,37 @@ const app = express();
 const http = require('http').createServer(app);
 const multiparty = require('multiparty');
 const path = require('path');
+const exec = require('child_process').exec;
+const request = require('request');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-var exec = require('child_process').exec;
+
 app.use(express.static('view'));
-
-const request = require('request');
-
-
-
 
 'use strict';
 
+// Google Cloud Platform project ID and Full path of the JSON file
+const projectId = 'propane-flag-230706';
+const keyFilename = 'FaceDetection-52a825bf3bf9.json';
 
-    const projectId = 'propane-flag-230706'; // Your Google Cloud Platform project ID
-    const keyFilename = 'FaceDetection-52a825bf3bf9.json'; //Full path of the JSON file
-
-    // Imports the Google Cloud client library
-    const {Storage} = require('@google-cloud/storage');
-
-    // Creates a client
-    const storage = new Storage({
-        projectId: projectId,
-        keyFilename: keyFilename
-    });
-    const bucket = storage.bucket('emousic_image');
-
-    var tmpName;
-
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage({
+    projectId: projectId,
+    keyFilename: keyFilename
+});
+const bucket = storage.bucket('emousic_image');
 
 
 app.post('/uploadImage', function(req,res){
 
-    //res.json({"music1":"song1"});
-
-    //res.send('aaa');
     const form = new multiparty.Form();
     form.parse(req,function(err,fields,files){
-        // console.log(fields);
         console.log(files);
-        // console.log(files['files'][0]['originalFilename']);
-        // const fileName = path.basename(files['files'][0]['originalFilename']);
-        // console.log(fileName);
-        // const file = bucket.file(fileName);
-        // console.log('file:' + file);
-        tmpName = files['files'][0]['path'].split("/").slice(-1)[0];
-        //console.log("tmpName： " + tmpName);
+        let tmpName = files['files'][0]['path'].split("/").slice(-1)[0];
         bucket.upload(files['files'][0]['path']).then( function v(){
             const imageUrl = `https://storage.googleapis.com/emousic_image/${tmpName}`;
-            //console.log(imageUrl);
             const subscriptionKey = '169d650d71f343c3b9c3ad7ac157e45a';
-
             const uriBase = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/detect';
-
-            //const imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg';
-            //const imageUrl = 'https://storage.googleapis.com/emousic_image/tqVLDY5ViY4M4Xi5SprXESXt.jpg';
-
-
             const params = {
                 'returnFaceId': 'true',
                 'returnFaceLandmarks': 'false',
@@ -76,23 +50,15 @@ app.post('/uploadImage', function(req,res){
                 }
             };
 
-
             request.post(options, (error, response, body) => {
 
                 if (error) {
                     console.log('Error:  request error', error);
                     return;
                 }
-                //console.log(response);
                 let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
                 jsonResponse = JSON.parse(jsonResponse);
-
-                //
-                // let exec = require('child_process').exec;
-                // console.log(jsonResponse);
-                // console.log()
-                //console.log(jsonResponse[0]);
-                if(typeof(jsonResponse[0]) == 'undefined'){
+                if(typeof(jsonResponse[0]) === 'undefined'){
                     console.log("there is no face detected in the picture");
                 }
                 else{
@@ -113,65 +79,25 @@ app.post('/uploadImage', function(req,res){
                     console.log("neutral:" + neutral);
                     console.log("sadness:" + sadness);
                     console.log("surprise:" + surprise);
-                    // exec('python emotion.py ' + anger + ' ' + contempt + ' ' + )
-                    exec('python emotion.py ' + anger + ' ' + contempt + ' '+disgust+' '+fear+' '+happiness+' '+neutral+' '+sadness+' '+surprise, (error,stdout,stderr)=>{
-                        console.log(stdout);
-
-                        var result = stdout;
-                        var valence = stdout.substring(stdout.indexOf(" ")+1, stdout.length - 3);
-                        var arsoual = stdout.substring(2, stdout.indexOf(" "));
-                        //console.log(stdout.substring(2, stdout.indexOf(" ")));
-                        //console.log(stdout.substring(stdout.indexOf(" ")+1, stdout.length - 3));
-                        exec('python getSong.py '+valence+' '+arsoual,(error,stdout,stderr)=>{
+                    exec('python emotion.py ' + anger + ' ' + contempt + ' ' + disgust + ' ' + fear + ' ' + happiness + ' ' + neutral + ' ' + sadness + ' ' + surprise, (error,stdout,stderr)=>{
+                        let valence = stdout.substring(stdout.indexOf(" ")+1, stdout.length - 3);
+                        let arousal = stdout.substring(2, stdout.indexOf(" "));
+                        console.log("valence: " + valence);
+                        console.log("arousal " + arousal);
+                        exec('python getSong.py '+valence+' '+arousal,(error,stdout,stderr)=>{
                             console.log(stdout);
                             if(error){
                                 console.log('error:' + error);
                             }
-                        })
+                        });
                         if(error){
                             console.log('error:' + error);
                         }
                     });
-
-                    
                 }
-
-                // $.ajax({
-                //     type: "POST",
-                //     url: "~/pythoncode.py",
-                //     data: {
-                //         anger: anger,
-                //         contempt: contempt,
-                //         disgust: disgust,
-                //         fear: fear,
-                //         happiness: happiness,
-                //         neutral: neutral,
-                //         sadness: sadness,
-                //         surprise: surprise
-                //     }
-                // }).done(function (av) {
-                //     // do something
-                //     console.log(av);
-                // });
-
+            });
         });
-        }
-
-
-
-
-        );
-        // noinspection JSAnnotator
-        // var waitTill = new Date(new Date().getTime() + 10 * 1000);
-        // while(waitTill > new Date()){}
-
-
-
-
     });
-
-
-
 });
 
 
@@ -182,7 +108,6 @@ app.get('/',function(req,res){
 });
 
 app.get('/takePhoto', (req,res) =>{
-    //console.log('aa');
     res.sendFile(path.resolve(__dirname+"/view/takePhoto.html"));
 });
 
